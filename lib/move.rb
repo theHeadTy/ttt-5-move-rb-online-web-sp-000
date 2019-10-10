81e874ab27ffac7312f895341f54621eaf4edb9f
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-10 16:24:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,8 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(user_input.to_i)
+def input_to_index(user_input)
   
-  user_input -= 1
-  
+  user.input.to_i -= 1
+
 end
