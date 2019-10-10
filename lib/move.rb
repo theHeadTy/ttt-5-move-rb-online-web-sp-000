0379e7540d64722b08c35cea8d37049ec64d8b9c
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-10 17:04:30 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board = Array.new(9, " "))
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
